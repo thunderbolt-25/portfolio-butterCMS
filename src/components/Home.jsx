f8408b830ce2ad150670524960c0b73053883e8e
thunderbolt-25 <anurags2503@gmail.com>
2024-02-04 00:27:53 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import logo from '../assets/logoex1.png';
+import { Link } from "react-scroll"; 
+
+const Home = () => {
+  return (
+    <div
+    name="home"
+    className="h-screen w-full bg-[#0a192f]"
+  >
+    <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+      <div className="flex flex-col justify-center h-full">
+        <h2 className="text-4xl sm:text-7xl font-bold text-white">
+          I'm a Full Stack Web Developer
+        </h2>
+        <p className="text-gray-500 py-4 max-w-md">
+        For a fresher stepping into the dynamic realm of web development, the journey begins with a passion for coding and a thirst for learning. As a newcomer, embracing the fundamentals of HTML, CSS, and JavaScript becomes the foundation for crafting digital experiences. With an eagerness to explore frameworks and technologies, a fresh web developer seeks to weave creativity into the fabric of the virtual world.
+        </p>
+        <div>
+          <Link
+            to="about"
+            smooth
+            duration={500}
+            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+          >
+            About Me
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight size={25} className="ml-3" />
+            </span>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <img
+          src={logo}  
+          alt="my profile"
+          className="rounded-2xl mx-auto w-2/3 md:w-full"
+        />
+      </div>
+    </div>
+  </div>
+  );
+};
+export default Home;
---
+++ src/components/Works.jsx
@@ -0,0 +1,172 @@
+import React from 'react';
+import code from '../assets/anna.jpg';
+import code1 from '../assets/votingscreen.jpg';
+import code2 from '../assets/bg_1.jpg';
+import code3 from '../assets/ecom2.avif';
+import code4 from '../assets/anna.jpg';
+import code5 from '../assets/anna.jpg';
+
+const Works = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8 w-full flex justify-center items-center flex-col'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
+            Work
+          </p>
+          <p className='py-6 text-2xl'>Check out some of my most recent work</p>
+        </div>
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${code})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Food Distribution
+              </span>
+              <p className='text-center'>A food web application built with Bootstrap and Mongodb</p>
+              <div className='pt-8 text-center'>
+                <a href='https://www.annapurna.epizy.com'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Website
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code1})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+              Online Voting App
+              </span>
+              <p className='text-center'>Built with Php and Bootstrap</p>
+              <div className='pt-8 text-center'>
+                <a href='https://phpvoting.epizy.com/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Website
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code2})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                Control Budget
+              </span>
+              <p className='text-center'>a web app to make a budget for any category</p>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/thunderbolt-25/ctrl-budget'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code3})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                E-commerce Store
+              </span>
+              <p className='text-center'>A online shopping web application built with php and Mongodb</p>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/thunderbolt-25/e-commerce-website'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code4})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                CBT Application
+              </span>
+              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${code5})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
+              <span className=' text-lg font-bold text-white tracking-wider'>
+                CBT Application
+              </span>
+              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Works;
