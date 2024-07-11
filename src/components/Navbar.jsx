2b3a3b66dcedd08efa725b96ffa283931cbd1fac
Anurag Singh <104016149+thunderbolt-25@users.noreply.github.com>
2024-07-11 23:45:55 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -94,7 +94,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://www.linkedin.com/in/anurag25'
+              href='https://www.linkedin.com/in/anurag25/'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
