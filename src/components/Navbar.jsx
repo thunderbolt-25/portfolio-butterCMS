61df9be62163b6824ae6dbd2ed0607bc6f0a4e16
Anurag Singh <104016149+thunderbolt-25@users.noreply.github.com>
2025-04-14 17:37:44 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,75 +1,119 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
 import {
-  FaBars,
-  FaTimes,
-  FaGithub,
-  FaLinkedin,
+FaBars,
+FaTimes,
+FaGithub,
+FaLinkedin,
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const handleClick = () => setNav(!nav);
+const [nav, setNav] = useState(false);
+const handleClick = () => setNav(!nav);
 
-  return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-gradient-to-r from-blue-900 via-purple-900 to-blue-900 text-gray-300 shadow-lg'>
-      <div>
-        <h1 className='font-thin text-2xl italic font-serif text-white'>My Portfolio</h1>
-      </div>
-      {/* menu */}
-      <ul className='hidden md:flex gap-x-8'>
-        {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
-          <li key={item} className='hover:text-white transition duration-300'>
-            <Link to={item} smooth={true} duration={500}>
-              {item.charAt(0).toUpperCase() + item.slice(1)}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10 cursor-pointer'>
-        {!nav ? <FaBars /> : <FaTimes />}
-      </div>
-      {/* Mobile menu */}
-      <ul
-        className={
-          !nav
-            ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-blue-900 to-purple-900 flex flex-col justify-center items-center'
-        }
-      >
-        {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
-          <li key={item} className='py-6 text-4xl text-white'>
-            <Link onClick={handleClick} to={item} smooth={true} duration={500}>
-              {item.charAt(0).toUpperCase() + item.slice(1)}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      {/* Social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-        <ul>
-          {[
-            { href: 'https://www.linkedin.com/in/anurag25/', icon: <FaLinkedin size={30} />, bg: 'bg-blue-600', label: 'Linkedin' },
-            { href: 'https://github.com/thunderbolt-25', icon: <FaGithub size={30} />, bg: 'bg-[#333333]', label: 'Github' },
-            { href: 'https://mail.google.com/mail/?view=cm&fs=1&to=anuragsingh.developers@gmail.com', icon: <HiOutlineMail size={30} />, bg: 'bg-[#6fc2b0]', label: 'Email' },
-            { href: 'https://drive.google.com/file/d/15DvnNGvIVLbFUazrKxJIk9a9RCE12TuJ/view?usp=sharing', icon: <BsFillPersonLinesFill size={30} />, bg: 'bg-[#565f69]', label: 'Resume' },
-          ].map(({ href, icon, bg, label }) => (
-            <li key={label} className={`w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ${bg}`}>
-              <a
-                className='flex justify-between items-center w-full text-gray-300'
-                href={href}
-              >
-                {label} {icon}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+return (
+<div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+<div>
+<h1 className=' font-thin text-2xl italic font-serif'> </h1>
+</div>
+{
+<ul className='hidden md:flex gap-x-8'>
+<li>
+<Link to='home' smooth={true} duration={500}>
+Home
+</Link>
+</li>
+<li>
+<Link to='about' smooth={true} duration={500}>
+About
+</Link>
+</li>
+<li>
+<Link to='skills' smooth={true} duration={500}>
+Skills
+</Link>
+</li>
+<li>
+<Link to='work' smooth={true} duration={500}>
+Work
+</Link>
+</li>
+<li>
+<Link to='contact' smooth={true} duration={500}>
+Contact
+</Link>
+</li>
+</ul>
+{/ Hamburger /}
+<div onClick={handleClick} className='md:hidden z-10'>
+{!nav ? <FaBars /> : <FaTimes />}
+</div>
+{/ Mobile menu /}
+<ul
+className={
+!nav
+? 'hidden'
+: 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+}
+>
+<li className='py-6 text-4xl'>
+<Link onClick={handleClick} to='home' smooth={true} duration={500}>
+Home
+</Link>
+</li>
+<li className='py-6 text-4xl'>
+{' '}
+<Link onClick={handleClick} to='about' smooth={true} duration={500}>
+About
+</Link>
+</li>
+<li className='py-6 text-4xl'>
+{' '}
+<Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+Skills
+</Link>
+</li>
+<li className='py-6 text-4xl'>
+{' '}
+<Link onClick={handleClick} to='work' smooth={true} duration={500}>
+Work
+</Link>
+</li>
+<li className='py-6 text-4xl'>
+{' '}
+<Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+Contact
+</Link>
+</li>
+</ul>
+{/ Social icons */}
+<div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+<ul>
+<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+<a className='flex justify-between items-center w-full text-gray-300' href='https://www.linkedin.com/in/anurag25/' >
+Linkedin <FaLinkedin size={30} />
+</a>
+</li>
+<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+<a className='flex justify-between items-center w-full text-gray-300' href='https://github.com/thunderbolt-25' >
+Github <FaGithub size={30} />
+</a>
+</li>
+<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+<a className='flex justify-between items-center w-full text-gray-300' href='https://mail.google.com/mail/?view=cm&fs=1&to=anuragsingh.developers@gmail.com' >
+Email <HiOutlineMail size={30} />
+</a>
+</li>
+<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+<a className='flex justify-between items-center w-full text-gray-300' href='https://drive.google.com/file/d/15DvnNGvIVLbFUazrKxJIk9a9RCE12TuJ/view?usp=sharing' >
+Resume <BsFillPersonLinesFill size={30} />
+</a>
+</li>
+</ul>
+</div>
+</div>
+);
 };
-
 export default Navbar;
